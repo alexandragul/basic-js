c050296013c965e8328a548fafa10c8d2b4cd770
alexandragul <72790705+alexandragul@users.noreply.github.com>
2021-02-22 15:53:35 +0300

    feat: implement function transform
    But advanced sequence interactions don't work well, control sequences don't work properly and the function changes initial array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  arr.forEach((item, index, array) => {
+    
+    if (item == '--discard-next') {
+
+      if (index == array.length)
+        array.splice(index, 1);
+
+      else array.splice(index, 1);
+    }
+
+    if (item == '--double-next') {
+
+      if (index == array.length)
+        array.splice(index, 1);
+
+      else array.splice(index, 1);
+    }
+
+    if (item == '--discard-prev') {
+
+      if (index == 0)
+        array.splice(0, 1);
+
+      else array.splice(index - 1, 1);
+    }
+
+    if (item == '--double-prev') {
+
+      if (index == 0)
+        array.splice(0, 1);
+        
+      else array.splice(index - 1, 1, array[index - 1]);
+    }
+  });
+
+  return arr;
 };
